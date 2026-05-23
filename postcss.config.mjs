193b4ff8d4bc63cf9d1e5c8ad1342de6b571802d
Charles-CS <charlesplaton263@gmail.com>
2026-05-23 12:48:57 +0800

    Replace V1 portfolio with new minimalist portfolio project architecture

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
@@ -7,10 +6,3 @@ const config = {
 }
 
 export default config
-=======
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
->>>>>>> 12b2230302204165003d88cc0e105bdee5f7813b
